2c9358025ac86ce4296c7accb0a798d64e7a6999
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-06-25 17:25:02 -0300

    segundoCommit

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function sobre()
+    {
+        return view('sobre'); // Garanta que você tem o arquivo sobre.blade.php em resources/views
+    }
+
+    public function videos()
+    {
+        return view('videos');
+    }
+
+    public function contato()
+    {
+        return view('contato');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
+// ... Suas outras rotas (Sobre, Videos, Contato) que fizemos antes ficam aqui em cima ...
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+// Suas outras rotas continuam normais abaixo...
+Route::get('/sobre', [SiteController::class, 'sobre'])->name('sobre');
+Route::get('/videos', [SiteController::class, 'videos'])->name('videos');
+Route::get('/contato', [SiteController::class, 'contato'])->name('contato');
+Route::get('/login', [AuthController::class, 'mostrarLogin'])->name('login');
+
+// Cole o código da Dashboard aqui no final do routes/web.php:
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
