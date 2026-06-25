60558c287e957b865a798486c1d1282002f4d767
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-06-25 16:22:44 -0300

    segundoCommit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nome'); 
+
+            $table->string('nome');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->date();
-            $table->
+
+            $table->date('data_nascimento')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
